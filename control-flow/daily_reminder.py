609bf42aa282d847de5bce1665dbc1bc27ceb924
Elton <eltonvurudi@gmail.com>
2025-09-21 13:42:02 +0300

    Add the changes in daily_remainder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,20 +2,22 @@ task = input("Enter your task:")
 priority = input("Priority(high/medium/low):")
 time_bound = input("Is it time-bound? (yes/no):")
 # Initialize the reminder message
-reminder = f"Reminder: {task}."
+reminder = f"Reminder:'{task}'"
 
 match priority:
     case 'high':
-        reminder += " This is a high-priority task."
+        reminder += "is a high-priority task"
     case 'medium':
-        reminder += " This is a medium-priority task."
+        reminder += "is a medium-priority task."
     case 'low':
-        reminder += " This is a low-priority task."
+        reminder += "is a low-priority task."
     case _:
-        reminder += " Priority level not recognized."
+        reminder += "Priority level not recognized."
 
 if time_bound == 'yes':
-    reminder += " This requires immediate attention today!"
+    reminder += "that requires immediate attention today!"
+else:
+    reminder += "Consider completing it when you have free time."
 
 print(reminder)
     
